58dbbcf5324f1736e73e0de8629293f67538a9ab
manish-9216 <95608590+manish-9216@users.noreply.github.com>
2022-04-14 18:38:24 +0530

    backup

---
+++ backup.py
@@ -0,0 +1,76 @@
+
+# for fixed text messages after a specific account sends messages ..................
+
+
+# if message.channel.name == "testing-channel":
+        
+#         if username=="Sunil":
+                          
+#             if user_message.lower():
+#                 await message.channel.send(f' no {username}!')
+#                 return
+        
+#..#######################################################################
+
+   
+# automated messages ..................................................
+   
+    # if message.channel.name == "testing-channel":
+        
+        
+        
+                             
+        # if user_message.lower()=="hello":
+        #     await message.channel.send(f'hello {username}!')
+        #     return
+       
+        
+        # elif user_message.lower()=="bye":
+        #     await message.channel.send(f'see you later {username}!')
+        #     return
+        
+        # elif user_message.lower()=="!random":
+        #     response=f'this is your random number : {random.randrange(10000000)}'
+        #     await message.channel.send(response)
+
+
+# #########################################################
+
+
+
+
+#sending messages at specific times....................................
+
+#@tasks.loop(minutes=1)
+# @client.event
+# # async def on_message(message):
+# async def test():
+#     # general_channel = client.get_channel(int(903145625305108541))
+#     general_channel = client.get_channel(int(961895973393682453))
+
+   
+#     print("Message sending on Discord channel")
+#     await general_channel.send("HELLO")
+
+
+
+
+# sending messages at random times...............................................
+
+# change---- #@tasks.loop(minutes=random.randint(3, 9))
+
+
+
+# randomly selecting content to send...........................................
+
+
+# text=["hi","hello","whats up","keep quiet"]
+
+# if message.channel.name == "testing-channel":
+        
+#                               
+#             if user_message.lower():
+#                 await message.channel.send(random.choice(text))
+#                 return
+
+###############################################################################
